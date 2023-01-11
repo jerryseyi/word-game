6a19d8463cf553e590d3e9135834501d5e4ee33e
Jerry Seyi <jerryseyi68@gmail.com>
2023-01-11 20:09:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+/*
+* procedure player class
+*   name, start = false;
+*   constructor(name, start = true)
+*
+*   setter: name
+*   getter: name
+*
+*
+*
+*   procedure class Randomize
+*
+* *   vowels = 'aeiou';
+*     consonants = 'bcdfghjklmnpqrstvwxyz'
+*     count = 0
+*     results = []
+*
+*       consonants(consonants):
+*           while counts < 2:
+*           val = randomize(consonants)
+*           results[] = val
+*           count++
+*
+*       vowels(vowels):
+*          while counts < 3:
+*           val = randomize(vowels)
+*           results[] = val
+*            count++
+*
+*
+*       words(result):
+*           random(result)
+*
+*
+*       procedure class InputWords inherit Randomize
+*
+*           meek - ee
+*           aqeemk - make - mma
+*           sum = 0
+*
+*           validate(char):
+*               if char in result:
+*                  count of the char.
+*                   if length specific char = 1
+*                       input
+*                       specific count = 0;
+*                   if length specific char > 1
+*                       input
+*                       specific count--
+*                   if length specific char = 0:
+*                       disable user input
+*               else:
+*                   throw error
+*
+*           valid(word):
+*               return isValid(word)
+*
+*           isAlphabets(word):
+*               if word is an alphabet:
+*                   return true
+*               return false
+*
+*          lowercase(word):
+*              return tolowercase(word)
+*
+*
+*
+*           isValid(word):
+*               check an endpoint
+*                   if word is found:
+*                       return true;
+*                    return false;
+*
+*
+*      procedure class AwardPoints
+* */
