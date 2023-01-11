18e97364b96f811b2cf2a90dffc5d4a51f521b22
Jerry Seyi <jerryseyi68@gmail.com>
2023-01-11 20:19:38 +0100

    player class started

--- app/player.js
+++ app/player.js
@@ -1,3 +1,21 @@
 class Player {
+    name;
+    start = false;
 
+    constructor(name, start = true) {
+        this.name = name;
+        this.start = start;
+    }
+
+    get name() {
+        return this.name;
+    }
+
+    set name(value) {
+        this.name = value;
+    }
 }
+
+player = new Player('Adedotun');
+player.name = 'Lateefat';
+console.log(player.name);
